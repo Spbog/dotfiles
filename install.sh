87149e7f0e81e60a50c449682acd1217f5b987da
avitoras <yamadeypon@gmail.com>
2024-09-22 21:44:38 +0300

    ну типо блять типо попробывал переписать скрипт и наговнокодил

--- install.sh
+++ install.sh
@@ -16,13 +16,27 @@ if [ -r /etc/os-release ]; then
     # если наш дистр это воид выполняем скрипт на инсталл пакетов для воида
     bash ~/dotfiles/scripts/void.sh
   else
-    echo "Наш скрипт пока не поддерживает ваш дистрибутив"
+    echo "Our script does not support your distro yet"
   fi
 else
-  echo "Нет прав доступа или файл /etc/os-release не найден."
+  echo "No access rights or the /etc/os-release file could not be found."
 fi
 
+bash ~/dotfiles/scripts/conf.sh
 
+	echo "Installation is complete! Enjoy using my dotfiles:)"
+	echo "Please do a reboot"
+	echo "In case of clarifying issues, please leave an issue in the github"
+	echo "https://github.com/Spbog/dotfiles"
+
+	read -p "Want to delete the git with dotfiles to free up space'? (y/n): " confirm
+	    if [ "$confirm" = "y" ]; then
+		rm -rf "~/dotfiles"
+		echo "The dotfiles directory has been deleted."
+	    else
+		echo "Ok, Ill keep it."
+            fi
+	    # ТЕПЕРЬ ДОРОГОЙ МОЙ ЮЗЕР МОЖЕШЬ ПОЙТИ И УБИТСЯ НАХУЙ НЕХУЙ В МОИХ ФАЙЛАХ КАПАЦА УЕБАН БЛЯТЬ ТУПОЙ
 
 
  
