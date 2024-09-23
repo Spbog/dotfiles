87d081cdae274944dfa3d6add35a7f917ec6d730
mxTeamCEO <143354480+mxTeamCEO@users.noreply.github.com>
2024-09-23 22:47:22 +0300

    Update archinstall.sh

--- install/archinstall.sh
+++ install/archinstall.sh
@@ -45,7 +45,7 @@ case "$proceed" in
 	echo "-------------" >> dot-install.log
 	sleep 0.5
 	# установка пакетов для дотов
-	sudo pacman -R pulseaudio
+	sudo pacman -R --noconfirm pulseaudio
 	yay -S --noconfirm hyprland rofi-wayland waybar hyprlock walogram-git pywal python3 python-pip python-pywalfox swww grim slurp mako emacs nautilus pipewire wireplumber pavucontrol helvum alacritty zoxide thefuck oh-my-posh  >> ~/dot-install.log
 		if ! yay -Qq hyprland rofi-wayland waybar hyprlock walogram-git pywal python3 python-pip python-pywalfox swww grim slurp mako emacs nautilus alacritty zoxide thefuck oh-my-posh &> /dev/null; then
 		echo "While installing the packages, some error occurred. Check the logs, and if you can't figure1 it out yourself: open an issue on github. The logs can be found at dot-install.log."
